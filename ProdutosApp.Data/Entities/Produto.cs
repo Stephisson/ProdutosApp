02f738587356ae115b97ee5fcb79a6f7c2dc0bea
Stephisson Galvao <stephissongalvao@fiotec.fiocruz.br>
2026-07-24 13:16:56 -0300

    Criação da entidade produto, repositório, consulta no banco de dados e controlador para serviços da API de produtos.

---
+++ ProdutosApp.Data/Entities/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProdutosApp.Data.Entities
+{
+    /// <summary> 
+    /// Modelo de entidade para produto. 
+    /// </summary>
+    public class Produto
+    {
+        #region Propriedades
+        public Guid Id { get; set; } = Guid.NewGuid(); 
+        public string Nome { get; set; } = string.Empty; 
+        public string Descricao { get; set; } = string.Empty; 
+        public decimal Preco { get; set; } = 0; 
+        public int Quantidade { get; set; } = 0; 
+        public DateTime DataCadastro { get; set; } = DateTime.Now; 
+        public bool Ativo { get; set; } = true;
+        #endregion
+    }
+}
