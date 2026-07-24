5243c58b666fc4b278370ad560dfbe5c16c3de70
Stephisson Galvao <stephissongalvao@fiotec.fiocruz.br>
2026-07-24 12:08:24 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutosApp.API/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Retorna OK (Sucesso)
+            return Ok("Consulta realizada com sucesso!");
+
+        }
+    }
+}
